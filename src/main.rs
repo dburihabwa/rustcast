eeca8d65462dbded4457581ce8b1db46817012ba
Dorian Burihabwa <dorian@burihabwa.com>
2021-08-01 18:54:03 +0200

    Split and merge an arbitrary file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+mod recast;
+
+use std::fs;
+use std::path::Path;
+
+const CHUNK_SIZE: usize = 4 * 1024 * 1204; // 4 MiB
+
 fn main() {
-    println!("Hello, World!");
+    let path = Path::new("src/main.rs");
+    let bytes = match fs::read(path) {
+        Err(why) => panic!("Could not read from {}: {}", path.display(), why),
+        Ok(bytes) => bytes
+    };
+    let chunks = recast::split(bytes.as_slice(), CHUNK_SIZE);
+    let merged = recast::merge(chunks);
+    assert_eq!(merged, bytes);
 }
